dc45ae3e583a95cce815311834724c33c79693c0
adam carter <adamcarter@hotmail.co.nz>
2020-03-30 15:11:03 +1300

    adding transition tests

---
+++ NUnitTests/TransitionTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using PowerpointAccessories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    class TransitionTest
+    {
+        //IIssueScanner scanner;
+        //IPowerpoint powerpoint;
+
+        [Test, TestCaseSource("testcases")]
+        public void AllAutoTransitionIssuesFound(IPowerpoint power)
+        {
+            IIssueScanner scanner = IssueScannerFactory.GetIssueScanner(power);
+            scanner.Scan();
+            Dictionary<string, SlideModel> slides = (Dictionary<string, SlideModel>)powerpoint.slides;
+            foreach (KeyValuePair<string, SlideModel> slide in slides)
+            {
+
+            }
+        }
+        //[OneTimeSetUp]
+        //public void Setup()
+        //{
+        //    powerpoint = PowerpointFactory.GetPowerpoint("C:\\Users\\conta\\OneDrive\\Desktop\\testCase1.pptx");
+        //    scanner = IssueScannerFactory.GetIssueScanner(powerpoint);
+        //}
+
+        static object[] testcases =
+        {
+        new object[]{PowerpointFactory.GetPowerpoint("C:\\Users\\conta\\OneDrive\\Desktop\\testCase1.pptx")},
+        new object[]{PowerpointFactory.GetPowerpoint("C:\\Users\\conta\\OneDrive\\Desktop\\testCase2.pptx")},
+        new object[]{PowerpointFactory.GetPowerpoint("C:\\Users\\conta\\OneDrive\\Desktop\\testCase3.pptx")},
+
+        };
+
+    }
+}
