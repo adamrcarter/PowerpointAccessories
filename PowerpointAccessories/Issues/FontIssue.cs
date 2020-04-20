b64a09004ea37823b4088bc362f92c98e49ee649
adam carter <adamcarter@hotmail.co.nz>
2020-04-20 14:17:54 +1200

    Bug fix fontissue file was ignored by git

---
+++ PowerpointAccessories/Issues/FontIssue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowerpointAccessories.Issues
+{
+    public class FontIssue : IssueItem
+    {
+        public string FontName { get; set; }
+        public string FontSupplier { get; set; }
+        public FontIssue(string fontSupplier, string fontName, string Description = "empty", bool IsFixable = false, bool IsChecked = false) : base(Description, IsFixable, IsChecked)
+        {
+            FontName = fontName;
+            FontSupplier = fontSupplier;
+
+        }
+    }
+}
